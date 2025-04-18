8302211c65e564e1a8c0aa66c5000e14202efe2e
KhenB <120479802+KhenB21@users.noreply.github.com>
2025-04-18 12:55:45 +0800

    First commit April 18, 2025

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('services/', views.services, name='Services'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# Create your views here.
+def home (request):
+    return render(request, 'index.html')
+def services (request):
+    return render(request, 'services.html')
+def register (request):
+    return render(request, 'register.html')
+def login (request):
+    return render(request, 'login.html')
